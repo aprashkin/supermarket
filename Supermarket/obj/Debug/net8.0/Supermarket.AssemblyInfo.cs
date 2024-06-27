3bf45167f3c005b78f0c7b81d6a3b70695fb2a68
SlavaBogdanov <ezh.bogov@mail.ru>
2024-06-27 14:49:24 +0300

    dewdw

--- Supermarket/obj/Debug/net8.0/Supermarket.AssemblyInfo.cs
+++ Supermarket/obj/Debug/net8.0/Supermarket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238fb83b8605be2a544bf4de3471b95331f1daa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f51f285363e6cda211bbfdc9858eecde7ce4cfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
